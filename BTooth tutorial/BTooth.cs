abc7834eec1a0392d350dae39d1cacc1f15fcda6
Damine0n <Damine0n@gmail.com>
2013-08-13 08:47:46 -0400

    add Everything

---
+++ BTooth tutorial/BTooth.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Net.Sockets;
+using InTheHand;
+using InTheHand.Net.Bluetooth;
+using InTheHand.Net.Ports;
+using InTheHand.Net.Sockets;
+
+
+
+namespace BTooth_tutorial
+{
+    public partial class BTooth : Form
+    {
+        List<string> items;
+        BluetoothDeviceInfo deviceInfo;
+        BluetoothDeviceInfo[] devices;
+        BluetoothClient client = new BluetoothClient();
+        string myPin = "1234";
+        Guid mUUID = new Guid("{00001101-0000-1000-8000-00805f9b34fb}");
+        bool serverStarted = false;
+        bool ready = false;
+        byte [] message;
+        public BTooth()
+        {
+            InitializeComponent();
+            items = new List<string>();
+        }
+
+        private void bGo_Click(object sender, EventArgs e)
+        {
+            tbOutput.Clear();
+            if (serverStarted)
+            {
+                updateUI("Server already started");
+                return;
+            }
+            if (rbClient.Checked)
+            {
+                startScan();
+            }
+            else
+            {
+                connectAsServer();
+            }
+        }
+
+        private void startScan()
+        {
+            listBox1.DataSource = null;
+            listBox1.Items.Clear();
+            items.Clear();
+            Thread bluetoothScanThread = new Thread(new ThreadStart(scan));
+            bluetoothScanThread.Start();
+        }
+
+        private void scan()
+        {            
+            updateUI("Starting Scan...");
+            
+            devices = client.DiscoverDevicesInRange();
+            updateUI("Scan complete");
+            updateUI(devices.Length.ToString() + " devices discovered");
+            foreach(BluetoothDeviceInfo d in devices)
+            {
+                items.Add(d.DeviceName);
+            }
+
+            updateDeviceList();
+        }
+        private void connectAsServer()
+        {
+            Thread bluetoothServerThread = new Thread(new ThreadStart(ServerConnectThread));
+            bluetoothServerThread.Start();
+        }
+        
+        
+        public void ServerConnectThread()
+        {
+            serverStarted = true;
+            updateUI("Server started, waiting for clients");
+            BluetoothListener blueListener = new BluetoothListener(mUUID);
+            blueListener.Start();
+            BluetoothClient conn = blueListener.AcceptBluetoothClient();
+            updateUI("Client has connected");
+
+            Stream mStream = conn.GetStream();
+            while(true)
+            {
+                try
+                {
+                    //handle server connection
+                    byte[] received = new byte[1024];
+                    mStream.Read(received, 0, received.Length);
+                    updateUI("Received " + Encoding.ASCII.GetString(received));
+                    byte[] sent = Encoding.ASCII.GetBytes("Hello World");
+                    mStream.Write(sent, 0, sent.Length);
+                } catch(Exception ex){
+                    updateUI("Client has disconnected!!!~!!!"+System.Environment.NewLine + ex);
+                }
+
+            }
+        }
+
+        private void updateUI(string message)
+        {
+            Func<int> del = delegate()
+            {
+                tbOutput.AppendText(message + System.Environment.NewLine);
+                return 0;
+            };
+            Invoke(del);
+        }
+
+        private void updateDeviceList()
+        {
+            Func<int> del = delegate()
+            {
+                listBox1.DataSource = items;
+                return 0;
+            };
+            Invoke(del);
+        }
+        private void listBox1_DoubleClick(object sender, EventArgs e)
+        {
+            
+            deviceInfo = devices.ElementAt(listBox1.SelectedIndex);
+
+            updateUI(deviceInfo.DeviceName + " was selected, attempting connect. "+System.Environment.NewLine+"Address = "+ deviceInfo.DeviceAddress);
+            if (pairDevice())
+            {
+                updateUI("device paired...");
+                Thread bluetoothClientThread = new Thread(new ThreadStart(ClientConnectThread));
+                bluetoothClientThread.Start();
+            }
+            else
+            {
+                updateUI("Pair Failed");
+            }
+        }
+        private void ClientConnectThread()
+        {
+            updateUI("attempting connection...");
+            client.BeginConnect(deviceInfo.DeviceAddress, mUUID,this.BluetoothClientConnectCallback,client);
+        }
+        void BluetoothClientConnectCallback(IAsyncResult result)
+        {
+            //if (result.IsCompleted)
+            try
+            {
+                BluetoothClient client = (BluetoothClient)result.AsyncState;
+                client.EndConnect(result);
+
+                Stream stream = client.GetStream();
+                stream.ReadTimeout = 1000;
+                updateUI("CONNECTED!!!");
+                //bGo.Enabled = false;
+            }
+            catch (Exception ex)
+            {
+                updateUI("Did Not Connect:"+System.Environment.NewLine+ex.ToString());
+            }
+        }
+        
+        private bool pairDevice()
+        {
+            if (!deviceInfo.Authenticated)
+            {
+                if (!BluetoothSecurity.PairRequest(deviceInfo.DeviceAddress, myPin))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private void tbText_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 13)
+            {
+                ready=true;
+                tbText.Clear();
+                Stream mStream = client.GetStream();
+                mStream.ReadTimeout = 4000;
+                try
+                {
+                    while (true)
+                    {                    
+                        //handle server connection
+                        message = Encoding.ASCII.GetBytes(tbText.Text);
+                        mStream.Write(message, 0, message.Length);
+                        byte[] received = new byte[4096];
+                        mStream.Read(received, 0, received.Length);
+                        updateUI("Received " + Encoding.ASCII.GetString(received));
+                    }
+                }
+                catch (Exception ex)
+                {
+                    updateUI("Client has disconnected!!!~!!!" + System.Environment.NewLine + ex);
+                    bGo.Enabled=true;
+                }
+            }
+        }
+    }
+}
---
+++ BTooth tutorial/SerialSearch.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using InTheHand.Net;
+using InTheHand.Net.Ports;
+using InTheHand.Net.Bluetooth;
+using InTheHand.Net.Sockets;
+using System.IO;
+using System.IO.Ports;
+
+
+namespace genericSerial
+{
+    public partial class SerialSearch : Form
+    {
+        private Queue<byte> recievedData = new Queue<byte>();
+        private string readBuffer = string.Empty;
+        private int Bytenumber;
+        private char[] byteEnd = new char[3];
+        static SerialPort _serial = null;
+        
+        public SerialSearch()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //richTextBox1.Text = serialPort1.Read().ToString();
+            System.Diagnostics.Debug.WriteLine("Test1");
+            Console.WriteLine("Test");
+            Console.Read();
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            if(serialPort1.IsOpen)
+            {
+                string text = textBox1.Text;
+                serialPort1.WriteLine(text);
+            }
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            listBox1.Items.Clear();
+            // Get a list of serial port names.
+            string[] ports = SerialPort.GetPortNames();
+
+            richTextBox1.Text += "The following serial ports were found:";
+
+            // Display each port name to the console. 
+            foreach (string port in ports)
+            {
+                listBox1.Items.Add(port);
+            }
+        }
+
+        private void listBox1_DoubleClick(object sender, EventArgs e)
+        {
+            int i=0;
+            string lines="";
+            List<string> buffer = new List<string>();
+            serialPort1.Close();
+            serialPort1.PortName = listBox1.SelectedItem.ToString();
+            listBox1.Items.Clear();
+            if (!serialPort1.IsOpen)
+            {
+                serialPort1.Open(); 
+                richTextBox1.Text += "\nOpened";
+                string text = textBox1.Text;
+                serialPort1.WriteLine(text);
+                while(i<10){
+                    serialPort1.DataReceived += new SerialDataReceivedEventHandler(serialPort1_DataReceived);
+                    lines+=serialPort1.ReadExisting()+"\n";
+                    i++;
+                }
+             }
+        }
+
+        private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {// this is a workaround because of an SDK bug
+            StringBuilder received = new StringBuilder();
+            while (_serial.BytesToRead > 0)
+                received.Append(ReadString());
+            //string received = ReadString();
+            // write the text out to the screen
+            richTextBox1.Clear();
+            richTextBox1.Text="Message from HQ:";
+            richTextBox1.Text+="\n"+received.ToString();
+            richTextBox1.Refresh();
+            // send back an acknowledgement
+            SendString("Agent 007 acknowledges message: " + received);
+        }
+        static string ReadString()
+        {
+            StringBuilder output = new StringBuilder();
+            int bufferSize = _serial.BytesToRead;
+            if (bufferSize > 0)
+            {
+                byte[] buffer = new byte[bufferSize];
+                int read = _serial.Read(buffer, 0, buffer.Length);
+                output.Append(System.Text.Encoding.UTF8.GetChars(buffer));
+                return output.ToString();
+            }
+            return "";
+        }
+        static void SendString(string message)
+        {
+            char[] charArray = message.ToCharArray();
+            byte[] byteArray = new byte[charArray.Length];
+            for (int i = 0; i < message.Length; i++)
+            {
+                byteArray[i] = (byte)charArray[i];
+            }
+            _serial.Write(byteArray, 0, charArray.Length);
+        }
+        //    //byte[] data = new byte[serialPort1.BytesToRead];
+        //    //serialPort1.Read(data,0,data.Length);
+
+        //    //data.ToList().ForEach(b => recievedData.Enqueue(b));
+        //    try
+        //    {
+        //        int count = serialPort1.BytesToRead;
+        //        byte[] ByteArray = new byte[count];
+        //        MessageBox.Show(serialPort1.Read(ByteArray, 0, count).ToString());
+        //        richTextBox1.Text = serialPort1.Read(ByteArray, 0, count).ToString();
+        //        byteEnd = serialPort1.NewLine.ToCharArray();
+
+        //        // get number off bytes in buffer 
+        //        Bytenumber = serialPort1.BytesToRead;
+
+        //        // read one byte from buffer 
+        //        //ByteToRead = SerialPort1.ReadByte() 
+
+        //        // read one char from buffer 
+        //        //CharToRead = SerialPort1.ReadChar() 
+
+        //        // read until string "90" 
+        //        //readBuffer1 = SerialPort1.ReadTo("90") 
+
+        //        // read entire string until .Newline  
+        //        readBuffer = serialPort1.ReadLine();
+        //        //data to UI thread 
+        //        this.Invoke(new EventHandler(DoUpdate));
+        //        processdata();
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        richTextBox1.Text="read " + ex.Message;
+        //    } 
+            
+        //}
+
+        //private void DoUpdate(object sender, EventArgs e)
+        //{
+        //    richTextBox1.Text += readBuffer;
+        //}
+
+        //private void processdata()
+        //{
+        //    // Determine if we have a "packet" in the queue
+        //    if(recievedData.Count > 5)
+        //    {
+        //        var packet = Enumerable.Range(0, 50).Select(i => recievedData.Dequeue());
+        //    }
+        //}
+
+        //public void Dispose()
+        //{
+        //    if (serialPort1 != null)
+        //        serialPort1.Dispose();
+        //}
+
+        private void SerialSearch_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (serialPort1.IsOpen)
+                serialPort1.Close();
+        }
+    }
+}
